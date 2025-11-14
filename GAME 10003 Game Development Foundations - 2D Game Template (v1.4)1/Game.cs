b37366ab4e9e44343cb7aa0590d710261b7d8f01
Luka Bairos <000975328@mohawkcollege.ca>
2025-11-13 20:10:14 -0500

    Create player and make them move

--- GAME 10003 Game Development Foundations - 2D Game Template (v1.4)1/Game.cs
+++ GAME 10003 Game Development Foundations - 2D Game Template (v1.4)1/Game.cs
@@ -11,14 +11,16 @@ namespace MohawkGame2D
     public class Game
     {
         // Place your variables here:
-
+        Vector2 playerPosition = new Vector2(380, 280); // Sets the initial player position
+        float playerSpeed = 200f; // Sets the player speed
+        float playerSize = 40f; // Sets the player size
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
-            Window.SetTitle("Just Shapes Jr."); // Sets the window title
+            Window.SetTitle("Just Squares Jr."); // Sets the window title
             Window.SetSize(800, 600); // Sets the window size (currently maximum)
         }
 
@@ -27,8 +29,23 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            // Set window background
+            Window.ClearBackground(Color.Black);
+
+            // Movement input
+            float moveX = Input.GetAxis(KeyboardInput.Left, KeyboardInput.Right); // Left and Right keys move the player respectively
+            float moveY = Input.GetAxis(KeyboardInput.Up, KeyboardInput.Down); // Up and Down keys move the player respectively
+
+            Vector2 movement = new Vector2(moveX, moveY);
+            playerPosition += movement * playerSpeed * Time.DeltaTime;
+
+            // Player graphic
+            Draw.FillColor = Color.Cyan;
+            Draw.Square(playerPosition, playerSize);
 
         }
+
+       
     }
 
 }
